2344edfe98324366ad6a47ccbd91ae1507d04c4d
NITHEESH-KUMAR-HUB <nitheeshkumar9433@gmail.com>
2025-11-20 00:48:29 +0530

    added extra necessary code

--- source/board.py
+++ source/board.py
@@ -54,6 +54,67 @@ class Board:
 
                     elif row > 4:         # red bottom rows
                         self.board[row][col] = Piece("RED", row, col)
+
+
+
+    def board_to_text(self):
+        lines = []
+        for row in range(8):
+            line = []
+            for col in range(8):
+                p = self.board[row][col]
+
+                if p is None:
+                    line.append("_")        # Empty
+                elif p.color == "RED":
+                    line.append("R" if p.king else "r")
+                elif p.color == "BLACK":
+                    line.append("B" if p.king else "b")
+
+            lines.append(" ".join(line))
+        return "\n".join(lines)
+
+
+    def save_board(self, filename="savefile.txt"):
+        text = self.board_to_text()
+        with open(filename, "w") as f:
+            f.write(text)
+        print(f"Board saved to {filename}")
+
+
+    def load_board(self, filename="savefile.txt"):
+        with open(filename, "r") as f:
+            rows = f.read().strip().split("\n")
+
+        for r in range(8):
+            cols = rows[r].split(" ")
+            for c in range(8):
+                char = cols[c]
+
+                if char == "_":
+                    self.board[r][c] = None
+
+                elif char == "r":
+                    self.board[r][c] = Piece("RED", r, c)
+
+                elif char == "R":
+                    p = Piece("RED", r, c)
+                    p.king = True
+                    self.board[r][c] = p
+
+                elif char == "b":
+                    self.board[r][c] = Piece("BLACK", r, c)
+
+                elif char == "B":
+                    p = Piece("BLACK", r, c)
+                    p.king = True
+                    self.board[r][c] = p
+
+        print(f"Board loaded from {filename}")
+
+
+
+
 board = Board()
 board.print_board() 
 
